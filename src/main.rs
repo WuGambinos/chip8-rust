d353518e9c4c59000ab6772c901d4c15c844a1b1
WuGambinos <lajuan.station@gmail.com>
2022-01-31 05:05:12 -0500

    Starting to implement instructions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use std::path::Path;
+use std::ffi::OsStr;
+use std::fs;
+use std::io::Error;
 struct Chip8 {
     memory: [u8; 4096],
+    display: [u8; 64*32],
     pc: u16,
     i: u16,
     stack: [u16; 16],
@@ -17,6 +22,7 @@ impl Chip8 {
     fn new() -> Self {
         Chip8 {
             memory: [0; 4096],
+            display: [0; 64 * 32],
             pc: 0x200,
             i: 0,
             stack: [0; 16],
@@ -32,8 +38,60 @@ impl Chip8 {
         }
     }
 
-    fn emulate_cycle(&self) {
-        let opcode = self.memory[self.pc as usize] <<  8 | self.memory[(self.pc+1) as usize];
+    fn emulate_cycle(&mut self) {
+
+        //Opcode 
+        self.opcode = (self.memory[self.pc as usize] <<  8) as u16 | (self.memory[(self.pc+1) as usize] as u16);
+        
+        //First byte of opcode
+        let first_byte: u16 = self.opcode & 0xF000;
+    }
+
+
+    fn op_0(&mut self) {
+
+        match self.opcode & 0x000F {
+            0x0000 => {
+                for i in 0..(64*32) {
+                    self.display[i as usize] = 0;
+                }
+                self.draw_flag = 1;
+                self.pc += 2;
+            },
+            0x000E => {
+                self.sp -= 1;
+                self.pc = self.stack[self.sp as usize];
+                self.pc += 2;
+            },
+
+            _ => println!("Unkown Opcode"),
+        };
+
+    }
+
+    fn op_1(&mut self) {
+        if self.opcode & 0x0FFF == self.pc {
+            self.halt = 1;
+            println!("INFINTE LOOP");
+        }
+
+        self.pc = self.opcode & 0x0FFF;
+    }
+
+    fn op_2(&mut self) {
+        self.stack[self.sp as usize] = self.pc;
+        self.sp += 1;
+        self.pc = self.opcode & 0xFFF;
+    }
+
+    fn op_3(&mut self) {
+        let x: u8 = ((self.opcode & 0xF00) >> 8) as u8; 
+
+        if(self.v[x as usize] == self.opcode as u8) {
+            self.pc += 4;
+        } else { 
+            self.pc += 2;
+        }
     }
 }
 
@@ -41,8 +99,31 @@ impl Chip8 {
 fn main() {
     println!("Hello, world!");
 
+    let path: &Path = Path::new("Fishie.ch8");
+    let rom = read_file(&path).unwrap();    
 
-    let chip = Chip8::new();
+    let mut chip: Chip8 = Chip8::new();
+    
+   /* for i in rom.iter() {
+        println!("{:#02x}", i);
+    }*/
 
-    println!("{:?}", chip.memory.len());
+    load_program(&mut chip, &rom);
+
+
 }
+
+
+fn read_file(path: &Path) -> Result<Vec<u8>, std::io::Error> {
+    let res = fs::read(path);
+
+    res
+}
+
+fn load_program(s: &mut Chip8, rom: &Vec<u8>){
+    for i in 0..rom.len() {
+        s.memory[(i+0x200) as usize] = rom[i];
+    }
+
+}
+
