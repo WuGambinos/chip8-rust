7176fbf75b2bc17faf9185f94d7fb47a9471dd6e
WuGambinos <lajuan.station@gmail.com>
2022-02-03 07:42:23 -0500

    Need to start testing

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,40 @@ use std::fs;
 pub use chip::Chip8;
 pub use chip::*;
 use std::path::Path;
-use bracket_lib::prelude::*;
+use raylib::prelude::*;
 
-impl GameState for Chip8 {
-    fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.print(1, 1, "Hello Bracket World");
-    }
-}
-fn main() -> BError {
+fn main() {
 
-    let path: &Path = Path::new("C:/Users/lajua/Desktop/programming/projects/chip8-rust/src/Fishie.ch8");
+    let path: &Path = Path::new("Fishie.ch8");
     let rom = read_file(&path).unwrap();
 
     let mut chip: Chip8 = Chip8::new();
+    chip.load_fontset();
     load_program(&mut chip, &rom);
-    chip.emulate_cycle();
 
-    let context = BTermBuilder::simple::<i32>(100, 50)?
-    .with_title("Hello Minimal Bracket World")
-    .build()?;
+    //Raylib
+    let (mut rl, thread) = raylib::init()
+    .size(640, 480)
+    .title("HEllo, World")
+    .build();
 
-    main_loop(context, chip)
+    rl.set_target_fps(60);
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::BLUE);
+        chip.emulate_cycle();
+
+        if chip.draw_flag == 1 {
+            chip.draw_graphics(&mut d);
+        }
+
+
+        d.draw_text("Hello, World!", 10, 10, 20, Color::WHITE);
+    }
+
+
 }
 
 fn read_file(path: &Path) -> Result<Vec<u8>, std::io::Error> {
